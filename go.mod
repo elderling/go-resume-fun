b13bcd4718cf58ee9075bf0cfff992522386f900
Jonathan W. Taylor <elderling@gmail.com>
2023-03-17 14:40:12 -0500

    Add some data and yaml marshalling

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module go-resume-fun
 
 go 1.19
 
-require github.com/go-pdf/fpdf v0.6.0 // indirect
+require (
+	github.com/go-pdf/fpdf v0.6.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,17 +1,54 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/go-pdf/fpdf"
+	"gopkg.in/yaml.v3"
 )
 
+type Job struct {
+	Title       string    `yaml:"title"`
+	Company     string    `yaml:"company"`
+	Location    string    `yaml:"location"`
+	Description string    `yaml:"description"`
+	StartDate   time.Time `yaml:"start_date"`
+	EndDate     time.Time `yaml:"end_date"`
+}
+
 func main() {
-	pdf := fpdf.New("P", "mm", "A4", "")
-	pdf.AddPage()
-	pdf.SetFont("Arial", "B", 16)
-	pdf.Cell(40, 10, "Hello, world")
-	err := pdf.OutputFileAndClose("hello.pdf")
+
+	austin, err := time.LoadLocation("America/Chicago")
 
 	if err != nil {
 		panic(err)
 	}
+
+	job := Job{
+		Title:       "Principal Software Architect",
+		Company:     "Switch",
+		Location:    "Austin, TX",
+		Description: "Transition one billing and account management system into multiple systems.",
+		StartDate:   time.Date(2022, time.January, 0, 0, 0, 0, 0, austin),
+		EndDate:     time.Date(2023, time.March, 0, 0, 0, 0, 0, austin),
+	}
+
+	pdf := fpdf.New("P", "mm", "A4", "")
+	pdf.AddPage()
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(40, 10, job.Title)
+	err2 := pdf.OutputFileAndClose("hello.pdf")
+
+	if err2 != nil {
+		panic(err2.Error())
+	}
+
+	theYaml, err3 := yaml.Marshal(job)
+
+	if err3 != nil {
+		panic(err3.Error())
+	}
+
+	fmt.Println(string(theYaml))
 }
